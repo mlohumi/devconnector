8607f4dfe6e34bc2ae63f4d7f85503a1891225bd
Mukesh Lohumi <mukesh@Mukeshs-MacBook-Air.local>
2020-08-10 02:41:57 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.send("<h1>API running successfully</h1>"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
